761149fe148000669313c81b6c3bbe47d3d9218f
Pinkie Pie <yarnovax@gmail.com>
2017-08-26 20:21:38 -0400

    Add S_EACH_SKILL_RESULT

---
+++ patterns/S_EACH_SKILL_RESULT.js
@@ -0,0 +1,6 @@
+module.exports = pak => {
+	let prev = pak.prev('S_ACTION_STAGE')
+
+	return prev && pak.parse() && pak.parsed.source.equals(prev.parsed.source) && pak.parsed.model === prev.parsed.model
+		&& pak.parsed.skill === prev.parsed.skill && pak.parsed.id === prev.parsed.id
+}
