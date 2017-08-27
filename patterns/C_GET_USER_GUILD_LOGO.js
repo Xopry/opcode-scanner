6e7d0c381196676a8dab1e1ea0c23b4875463e16
Pinkie Pie <yarnovax@gmail.com>
2017-08-26 21:55:25 -0400

    Avoid false positives for C_GET_USER_GUILD_LOGO

--- patterns/C_GET_USER_GUILD_LOGO.js
+++ patterns/C_GET_USER_GUILD_LOGO.js
@@ -1,1 +1,11 @@
-module.exports = pak => pak.order >= 4 && pak.parse() && pak.parsed.playerId !== 0 && pak.parsed.guildId !== 0
+module.exports = pak => {
+	let prev = pak.prev('S_GET_USER_LIST')
+
+	if(!prev || !pak.parse()) return false
+
+	for(let character of prev.parsed.characters)
+		if(pak.parsed.playerId === character.id && pak.parsed.guildId === character.guildId)
+			return true
+
+	return false
+}
