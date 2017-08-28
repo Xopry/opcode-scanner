1e764cadbf72d8c967a7fee1f16a3287897b4f30
Pinkie Pie <yarnovax@gmail.com>
2017-08-27 21:24:18 -0400

    Update S_EACH_SKILL_RESULT for projectiles

--- patterns/S_EACH_SKILL_RESULT.js
+++ patterns/S_EACH_SKILL_RESULT.js
@@ -1,6 +1,6 @@
 module.exports = pak => {
 	let prev = pak.prev('S_ACTION_STAGE')
 
-	return prev && pak.parse() && pak.parsed.source.equals(prev.parsed.source) && pak.parsed.model === prev.parsed.model
-		&& pak.parsed.skill === prev.parsed.skill && pak.parsed.id === prev.parsed.id
+	return prev && pak.parse() && (!pak.parsed.owner.equals(0) ? pak.parsed.owner : pak.parsed.source).equals(prev.parsed.source)
+		&& pak.parsed.model === prev.parsed.model && pak.parsed.skill === prev.parsed.skill && pak.parsed.id === prev.parsed.id
 }
