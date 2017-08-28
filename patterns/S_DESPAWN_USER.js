b05ed9501474f21d69ba48d79eed05b426419fb0
Pinkie Pie <yarnovax@gmail.com>
2017-08-27 22:35:47 -0400

    Correct S_DESPAWN_USER signature

--- patterns/S_DESPAWN_USER.js
+++ patterns/S_DESPAWN_USER.js
@@ -1,5 +1,5 @@
 module.exports = pak => {
-	if(!pak.parse()) return false
+	if(!pak.parse() || (pak.parsed.type !== 0 && pak.parsed.type !== 1)) return false
 
 	for(let i = pak.index - 1; i >= 0; i--) {
 		let prev = pak.history[i]
