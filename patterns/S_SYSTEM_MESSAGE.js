ec19f44783cc37089bc243ff25791c93c43880fa
Pinkie Pie <yarnovax@gmail.com>
2017-09-02 17:10:44 -0400

    Correct S_SYSTEM_MESSAGE

--- patterns/S_SYSTEM_MESSAGE.js
+++ patterns/S_SYSTEM_MESSAGE.js
@@ -1,1 +1,1 @@
-module.exports = pak => pak.mapped('S_MY_DESCRIPTION') && pak.parse() && /^@\d+/.test(pak.parsed.message)
+module.exports = pak => pak.mapped['S_MY_DESCRIPTION'] && pak.parse() && /^@\d+/.test(pak.parsed.message)
