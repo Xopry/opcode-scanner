e060014722b0c01fd37108ded7d657410e1a8f75
Pinkie Pie <yarnovax@gmail.com>
2017-09-03 07:56:45 -0400

    Update S_LOAD_CLIENT_ACCOUNT_SETTING

--- patterns/S_LOAD_CLIENT_ACCOUNT_SETTING.js
+++ patterns/S_LOAD_CLIENT_ACCOUNT_SETTING.js
@@ -1,5 +1,1 @@
-module.exports = pak => {
-	let prev = pak.prev()
-
-	return prev && prev.name() === 'S_GET_USER_LIST'
-}
+module.exports = pak => pak.prev('S_GET_USER_LIST') && pak.parse() && pak.parsed.data.length
